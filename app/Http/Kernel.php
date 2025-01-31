22cd7bfeb000c7227a086c5c695a78c7a14e5565
Alfian <ara@Alfians-MacBook-Air.local>
2025-01-31 19:44:21 +0700

    api fixing

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -1,12 +1,13 @@
 protected $middleware = [
     \Illuminate\Http\Middleware\HandleCors::class,
-    // ... other middleware
+    \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+    \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
 ];
 
 protected $middlewareGroups = [
     'api' => [
         \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
         \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        \Illuminate\Http\Middleware\HandleCors::class,
+        // Removed duplicate CORS middleware from here
     ],
 ];
