9ceec2ea70e2623a608819df87cc445f8bc88bef
Alfian <ara@Alfians-MacBook-Air.local>
2025-01-31 19:47:22 +0700

    api fixing2

--- config/cors.php
+++ config/cors.php
@@ -2,10 +2,10 @@
 
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-    'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],  // In production, change this to your specific domain
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    'allowed_origins' => ['https://raodlatul.my.id'],
     'allowed_origins_patterns' => [],
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['Content-Type', 'X-Requested-With', 'Authorization', 'X-CSRF-TOKEN'],
     'exposed_headers' => [],
     'max_age' => 0,
     'supports_credentials' => true,
